560e7348908e5a96f417e3be6d5bb69b40f4c59e
BipulLamsal <bipullamsal@gmail.com>
2023-10-06 21:45:22 +0545

    description automation implemented

--- hackfest-automation/code.js
+++ hackfest-automation/code.js
@@ -1,8 +1,20 @@
 //main function that runs at first
 //make sure to have only one component in the page of type COMPONENT  (only work for this!)
-//select only one item and run the plugin 
+//select only one item and run the plugin
 async function runPlugin() {
   //custom api request allowing cros form localhost
+  await figma.loadFontAsync({ family: "Poppins", style: "Light" });
+  await figma.loadFontAsync({ family: "Poppins", style: "Bold" });
+  const page = figma.currentPage.selection[0];
+  if(page.name==="JUDGE")
+  {
+
+  }
+  else if(page.name==="TEAM")
+  {
+    
+  }
+
   const apiURL = "http://localhost:1939/api/teams";
   try {
     const response = await fetch(apiURL);
@@ -13,7 +25,9 @@ async function runPlugin() {
 
     const certificate = figma.currentPage.selection[0];
     if (!certificate || certificate.type !== "COMPONENT") {
-      figma.notify("Make sure to select a design which should be COMPONENT typed");
+      figma.notify(
+        "Make sure to select a design which should be COMPONENT typed"
+      );
       figma.closePlugin();
       return;
     }
@@ -22,7 +36,38 @@ async function runPlugin() {
       //cloning the document based on the data
       await cloneDocument(certificate, data.data);
     } else {
-      console.log(data.data)
+      console.log(data.data);
+      console.error("Invalid data format. Expected an array.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    figma.closePlugin();
+  }
+}
+
+async function getAPIrequest(apiURL)
+{
+  try {
+    const response = await fetch(apiURL);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    //selects the first index of the selected component
+    const certificate = figma.currentPage.selection[0];
+    if (!certificate || certificate.type !== "COMPONENT") {
+      figma.notify(
+        "Make sure to select a design which should be COMPONENT typed"
+      );
+      figma.closePlugin();
+      return;
+    }
+    //allowing only array from the api response
+    if (Array.isArray(data.data)) {
+      //cloning the document based on the data
+      await cloneDocument(certificate, data.data);
+    } else {
       console.error("Invalid data format. Expected an array.");
     }
   } catch (error) {
@@ -34,12 +79,99 @@ async function runPlugin() {
 
 async function configDocument(certificate, parsedData) {
   for (const layer of certificate.children) {
-    if (layer.name == 'TEAMNAME') {
+    if (layer.name == "NAME") {
       const fontName = layer.fontName;
+      console.log(fontName.family);
       //async waiting the text layer to load the font object and applying filter (main async required!!)
-      await figma.loadFontAsync({ family: fontName.family, style: fontName.style });
+      await figma.loadFontAsync({
+        family: fontName.family,
+        style: fontName.style,
+      });
       //executes once the font is fully loaded
       layer.characters = parsedData.Name;
+    }
+    if (layer.name == "DESCRIPTION") {
+      const text = layer.characters;
+      const fontStylesPerWord = [];
+      let currentWord = "";
+      let currentFontFamily = null;
+      let currentFontWeight = null;
+      let currentFontColor = null;
+      let isSpace = false;
+
+      for (const letter of text) {
+        if (letter === " " || letter === "\n" || letter === "\t") {
+          isSpace = letter;
+        } else {
+          currentWord += letter;
+
+          const fontName = layer.getRangeFontName(
+            text.indexOf(letter),
+            text.indexOf(letter) + 1
+          );
+          if (fontName.family !== currentFontFamily) {
+            currentFontFamily = fontName.family;
+          }
+          if (fontName.style.includes("Bold") && currentFontWeight !== "Bold") {
+            currentFontWeight = "Bold";
+          } else if (
+            !fontName.style.includes("Bold") &&
+            currentFontWeight !== "Light"
+          ) {
+            currentFontWeight = "Light";
+          }
+        }
+
+        if (isSpace === " " || isSpace === "\n" || isSpace === "\t") {
+          if (currentWord) {
+            if (currentWord === "[REPLACETEAM]") {
+              currentWord = parsedData.Team;
+            }
+            fontStylesPerWord.push({
+              word: currentWord,
+              fontFamily: currentFontFamily,
+              fontWeight: currentFontWeight,
+              fontColor,
+            });
+            fontStylesPerWord.push({
+              word: isSpace,
+              fontFamily: currentFontFamily,
+              fontWeight: currentFontWeight,
+            });
+          }
+          currentWord = "";
+          isSpace = false;
+        }
+      }
+      if (currentWord) {
+        fontStylesPerWord.push({
+          word: currentWord,
+          fontFamily: currentFontFamily,
+          fontWeight: currentFontWeight,
+        });
+      }
+      let insertPosition = 0;
+      console.log(fontStylesPerWord);
+
+      // Iterate through the fontStylesPerWord array and insert characters into the layer
+      layer.characters = "";
+      for (const item of fontStylesPerWord) {
+        const wordLength = item.word.length;
+        const insertText = item.word;
+
+        await figma.loadFontAsync({ family: "Alex Brush", style: "Regular" });
+        // Replace text in the layer at the specified position with the new word and font style
+        layer.insertCharacters(insertPosition, insertText);
+
+        // Set the font style for the inserted word
+        layer.setRangeFontName(insertPosition, insertPosition + wordLength, {
+          family: item.fontFamily,
+          style: item.fontWeight,
+        });
+
+        // Update the insert position for the next word
+        insertPosition += wordLength;
+      }
     }
   }
 }
--- hackfest-automation/index.js
+++ hackfest-automation/index.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 const port = 1939;
-const FILENAMES = ["ORGTEAMS.csv"];
+//make sure not to change the order of the file
+//donot change the order!!
+const FILENAMES = ["CSVs/ORGTEAMS.csv", "CSVs/JUDGES.csv"];
 // this file includes reading the csv file. Make sure to change file name as required
 const fs = require("fs");
 const papa = require("papaparse");
@@ -12,7 +14,7 @@ function parseFile(file) {
   //file name is mentioned here
   fs.readFile(file, "utf-8", (err, data) => {
     if (err) {
-      console.log("error reading the csv file for : ",file, err);
+      console.log("error reading the csv file for : ", file, err);
       return;
     }
     parseCSV(data, file);
@@ -22,8 +24,8 @@ function parseCSV(data, file) {
   papa.parse(data, {
     header: true,
     skipEmptyLines: true,
-    complete: function(results){
-      routeHandler(results, file)
+    complete: function (results) {
+      routeHandler(results, file);
     },
     error: function (err) {
       console.log("Parsing Error", err.message);
@@ -41,7 +43,7 @@ function routeHandler(results, file) {
       });
       break;
     case 1:
-      app.get("/api/participants", (req, res) => {
+      app.get("/api/judges", (req, res) => {
         res.set("Access-Control-Allow-Origin", "*");
         res.json(results);
       });
@@ -49,9 +51,9 @@ function routeHandler(results, file) {
   }
 }
 
-FILENAMES.forEach((item)=>{
+FILENAMES.forEach((item) => {
   parseFile(item);
-})
+});
 app.listen(port, () => {
   console.log(`server runnign at PORT ${port}`);
 });
